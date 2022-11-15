e6093ba6f9491c0828a12b6de5b0b1080a5b4dea
Remi <118309884+Remi-VC@users.noreply.github.com>
2022-11-15 16:20:57 -0500

    Initial Code Commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class App extends React.PureComponent{
+    public render(): React.ReactNode {
+        return (
+            <div>Work in progress</div>
+        );
+    }
+}
